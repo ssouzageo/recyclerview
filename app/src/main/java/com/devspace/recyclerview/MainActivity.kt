7d1a9e335275d249a66b96bea2382c943e2e27d4
ssouzageo <ssouzageovaneo@gmail.com>
2025-04-04 15:35:27 -0300

    Atualização.

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -3,18 +3,103 @@ package com.devspace.recyclerview
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        enableEdgeToEdge()
     }
+
+    // Create data class
+    // Create list From data class type
+    // Create Adapter
+    // Set Adapter
+    // Linear layout manager
+
+    val rvList = findViewById<RecyclerView>(R.id.rv_list)
 }
+
+val contacts = listOf(
+    Contact(
+        "Geo",
+        "(82) 988987412",
+        R.drawable.sample1
+    ),
+    Contact(
+        "Janaina",
+        "(82) 956321478",
+        R.drawable.sample2
+    ),
+    Contact(
+        "Jane Cléa",
+        "(82) 978456321",
+        R.drawable.sample3
+    ),
+    Contact(
+        "Edmilson",
+        "(82) 978451236",
+        R.drawable.sample4
+    ),
+    Contact(
+        "Elza",
+        "(82) 932569874",
+        R.drawable.sample5
+    ),
+    Contact(
+        "JP",
+        "(82) 923565874",
+        R.drawable.sample6
+    ),
+    Contact(
+        "Maurício",
+        "(82) 912457895",
+        R.drawable.sample7
+    ),
+    Contact(
+        "Elvis",
+        "(82) 945698745",
+        R.drawable.sample8
+    ),
+    Contact(
+        "Gustavo",
+        "(82) 987456325",
+        R.drawable.sample9
+    ),
+    Contact(
+        "Neguet",
+        "(82) 945697898",
+        R.drawable.sample10
+    ),
+    Contact(
+        "Ariel",
+        "(82) 923569874",
+        R.drawable.sample11
+    ),
+    Contact(
+        "Júlio Cezar",
+        "(82) 912456532",
+        R.drawable.sample12
+    ),
+    Contact(
+        "Neto",
+        "(82) 912321256",
+        R.drawable.sample13
+    ),
+    Contact(
+        "Alex",
+        "(82) 945784563",
+        R.drawable.sample14
+    ),
+    Contact(
+        "Alan",
+        "(82) 945452132",
+        R.drawable.sample15
+    ),
+    Contact(
+        "Lucas",
+        "(82) 945126358",
+        R.drawable.sample16
+    )
+)
